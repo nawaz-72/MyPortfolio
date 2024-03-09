09a8bcf036a393a9190bbabd3a91389e3cea4fc1
nawaz-72 <nawaz72000@gmail.com>
2024-03-09 16:15:06 +0500

    set my working page

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -4,40 +4,40 @@ const workSlides = {
     {
       images: [
         {
-          title: 'title',
-          path: '/thumb1.jpg',
+          title: "title",
+          path: "/thumb1.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb2.jpg',
+          title: "title",
+          path: "/thumb2.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb3.jpg',
+          title: "title",
+          path: "/thumb3.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb4.jpg',
+          title: "title",
+          path: "/thumb4.jpg",
         },
       ],
     },
     {
       images: [
         {
-          title: 'title',
-          path: '/thumb4.jpg',
+          title: "title",
+          path: "/thumb4.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb1.jpg',
+          title: "title",
+          path: "/thumb1.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb2.jpg',
+          title: "title",
+          path: "/thumb2.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb3.jpg',
+          title: "title",
+          path: "/thumb3.jpg",
         },
       ],
     },
@@ -45,56 +45,60 @@ const workSlides = {
 };
 
 // icons
-import {
-  BsArrowRight,
-} from "react-icons/bs";
+import { BsArrowRight } from "react-icons/bs";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
 
-import {Swiper, SwiperSlide} from "swiper/react"
-import {Pagination} from "swiper"
-
-import 'swiper/css'
-import "swiper/css/free-mode"
-import "swiper/css/pagination"
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
 import Image from "next/image";
 
-
-
 const WorkSlider = () => {
-  return <Swiper 
-  spaceBetween={10}
-
-  pagination={{
-    clickable: true
-  }}
-  modules={[Pagination]}
-  className="h-[280px] sm:h-[480px]">
-    {workSlides.slides.map((slide, itemIndex) => {
-      return (
-        <SwiperSlide key={itemIndex}>
-          <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
-            {slide.images.map((image, index) => {
-              return(
-                <div className="relative rounded-lg overflow-hidden flex items-center justify-center group" key={index}>
-                <div className="flex items-center justify-center relative overflow-hidden group">
-                  <Image src={image.path} width={500} height={500} alt=""
-                  />
-                  <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
-                  <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-                    <div className="flex items-center gap-x-2 text-xs tracking-[0.2ch]">
-                      <div className="delay-100">LIVE</div>
-                      <div className="translate-y-[500%] group-hover:translate-y-0 transition-all delay-150 duration-300">PROJECT</div>
-                      <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all delay-200 duration-300 "><BsArrowRight/></div>
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="h-[280px] sm:h-[460px] md:h-[300px] lg:h-[460px] xl:h-[110%]"
+    >
+      {workSlides.slides.map((slide, itemIndex) => {
+        return (
+          <SwiperSlide key={itemIndex}>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+              {slide.images.map((image, index) => {
+                return (
+                  <div
+                    className="relative rounded-lg overflow-hidden flex items-center justify-center group"
+                    key={index}
+                  >
+                    <div className="flex items-center justify-center relative overflow-hidden group">
+                    <Image src={image.path} width={500} height={500} alt="projectPic" />
+                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
+                        <div className="flex items-center gap-x-2 text-xs tracking-[0.2ch]">
+                          <div className="delay-100">LIVE</div>
+                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all delay-150 duration-300">
+                            PROJECT
+                          </div>
+                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all delay-200 duration-300 ">
+                            <BsArrowRight />
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>) 
-            })}
-          </div>
-        </SwiperSlide>
-      )
-    })}
-  </Swiper>;
+                );
+              })}
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default WorkSlider;
