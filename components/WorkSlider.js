4057ccb854b16d16fe50df35520cfb1b92601d5b
Nawaz <nawaz72000@gmail.com>
2024-03-25 15:21:02 +0500

    upload content on working page

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -4,20 +4,20 @@ const workSlides = {
     {
       images: [
         {
-          title: "title",
-          path: "/thumb1.jpg",
+          title: "Theta Vibes",
+          path: "/check2.jpg",
         },
         {
           title: "title",
-          path: "/thumb2.jpg",
+          path: "/check2.jpg",
         },
         {
           title: "title",
-          path: "/thumb3.jpg",
+          path: "/check2.jpg",
         },
         {
           title: "title",
-          path: "/thumb4.jpg",
+          path: "/check2.jpg",
         },
       ],
     },
@@ -25,19 +25,19 @@ const workSlides = {
       images: [
         {
           title: "title",
-          path: "/thumb4.jpg",
+          path: "/check2.jpg",
+        },
+        {
+          title: "Theta Vibes",
+          path: "/check2.jpg",
         },
         {
           title: "title",
-          path: "/thumb1.jpg",
+          path: "/check2.jpg",
         },
         {
           title: "title",
-          path: "/thumb2.jpg",
-        },
-        {
-          title: "title",
-          path: "/thumb3.jpg",
+          path: "/check2.jpg",
         },
       ],
     },
@@ -63,12 +63,12 @@ const WorkSlider = () => {
         clickable: true,
       }}
       modules={[Pagination]}
-      className="h-[280px] sm:h-[460px] md:h-[300px] lg:h-[460px] xl:h-[110%]"
+      className="h-[300px] md:h-[280px] lg:h-[360px] xl:h-[110%] "
     >
       {workSlides.slides.map((slide, itemIndex) => {
         return (
           <SwiperSlide key={itemIndex}>
-            <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+            <div className="grid grid-cols-1 grid-rows-1  lg:grid-cols-2 lg:grid-rows-2 gap-4 cursor-pointer">
               {slide.images.map((image, index) => {
                 return (
                   <div
@@ -76,10 +76,10 @@ const WorkSlider = () => {
                     key={index}
                   >
                     <div className="flex items-center justify-center relative overflow-hidden group">
-                    <Image src={image.path} width={500} height={500} alt="projectPic" />
+                    <Image className="w-full object-fill" src={image.path} width={300} height={300} alt="projectPic" />
                       <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e92323] to-[#360abb] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
                       <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-                        <div className="flex items-center gap-x-2 text-xs tracking-[0.2ch]">
+                        <div className="flex items-center gap-x-2 text-[8px] lg:text-xs tracking-[0.2ch]">
                           <div className="delay-100">LIVE</div>
                           <div className="translate-y-[500%] group-hover:translate-y-0 transition-all delay-150 duration-300">
                             PROJECT
